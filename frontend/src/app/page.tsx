27bbb502efcb02891b6615cadc3cea400597fccf
Anita <writeanita.n@gmail.com>
2025-08-30 16:33:08 +0100

    add sidebar layout and main chat structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,21 @@
-
 export default function Home() {
-  <main>
-  <h1>Welcome to my app 🚀</h1>
-</main>
+  return (
+    <div className="main-sidebar">
+      <aside className="sidebar">
+        <h2 className="side-text">Naija Constitution Ai</h2>
+        <a href="#">New Chat</a>
+        <a href="#">History</a>
+        <a href="#">Settings</a>
+      </aside>
+    <main className="main">
+      <div className="center-content">
+        <div className="chat-logo"></div>
+        <h1 className="greeting">Hello there,<br />How can I assist you today?</h1>
+      </div>
+      <div className="search-container">
+        <input type="text" placeholder="Ask me anything..." className="search-input"/>
+      </div>
+    </main>
+    </div>
+  );
 }
